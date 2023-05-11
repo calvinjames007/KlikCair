3484e623f94dbf0772909fc8ac1c522f58fed908
dante4rt <rxmxdhxni@gmail.com>
2023-05-11 14:59:44 +0700

    rama's part

--- routers/user.js
+++ routers/user.js
@@ -8,6 +8,19 @@ router.post('/add', Controller.addNewUser)
 router.get('/login', Controller.showFormLogin)
 router.post('/login', Controller.postLogin)
 
+router.use((req, res, next) => {
+    console.log(req.session);
+
+    if (!req.session.UserId) {
+        const errors = `Please login first.`
+        res.redirect(`/user/login?errors=${errors}`)
+    } else {
+        next()
+    }
+  })
+
 router.get('/dashboard', Controller.showDashboard)
+
+router.get('/logout', Controller.logout)
 
 module.exports = router
